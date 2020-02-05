8e2368c440aef01ab381f17b15eef4fff7b498c2
Kunal Dubey <kunaldubey2297@gmail.com>
2020-02-05 20:06:43 +0530

    Home Page UI

--- src/components/ConfirmAccountPage/index.js
+++ src/components/ConfirmAccountPage/index.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react"; 
-import ReactDOM from 'react-dom'
-import MaterialTable from 'material-table'
-import { makeStyles } from "@material-ui/core"; 
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import MaterialTable from "material-table";
+import { makeStyles } from "@material-ui/core";
 
-import usersData from '../../data/users.json';
+import usersData from "../../data/users.json";
 
-const tableStyles = makeStyles({ 
-    CheckIcon:{ 
-        color:"green"
-    }
-})
+const tableStyles = makeStyles({
+  CheckIcon: {
+    color: "green"
+  }
+});
 
 export class ConfirmAccount extends Component {
   render() {
@@ -20,33 +20,33 @@ export class ConfirmAccount extends Component {
           columns={[
             { title: "Name", field: "firstName" },
             { title: "Batch", field: "batch" },
-            { title: "Roll Number", field: "rollNumber"}, 
-            { title: "Phone Number", field: "phoneNumber"}, 
-            { title: "Email", field: "email"},
+            { title: "Roll Number", field: "rollNumber" },
+            { title: "Phone Number", field: "phoneNumber" },
+            { title: "Email", field: "email" }
           ]}
-          data={usersData.filter( user => !user.verified)}
+          data={usersData.filter(user => !user.verified)}
           actions={[
             {
               icon: "check",
               tooltip: "Accept",
-              onClick: (event, rowData) => {  
-                const uid = rowData.uId  
-                usersData.forEach(user => { 
-                  if( user.uId === uid ){ 
-                    user.verified = true
+              onClick: (event, rowData) => {
+                const uid = rowData.uId;
+                usersData.forEach(user => {
+                  if (user.uId === uid) {
+                    user.verified = true;
                   }
                 });
-                }, 
-              color:'primary', 
-              iconProps: { style: {  color: "green" } },
-            }, 
+              },
+              color: "primary",
+              iconProps: { style: { color: "green" } }
+            },
             {
-                icon: "clear",
-                tooltip: "Reject",
-                onClick: (event, rowData) => alert("Request Rejected "), 
-                color:'primary', 
-                iconProps: { style: {  color: "red" } },
-              }
+              icon: "clear",
+              tooltip: "Reject",
+              onClick: (event, rowData) => alert("Request Rejected "),
+              color: "primary",
+              iconProps: { style: { color: "red" } }
+            }
           ]}
         />
       </div>
