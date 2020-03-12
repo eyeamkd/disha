9eb06d514a35f93e79d3cfd01a4c1bc5b2788af8
eyeamkd <Kunal@2297>
2020-03-12 22:10:33 +0530

    New Post Form Design

--- src/components/NewPost/index.js
+++ src/components/NewPost/index.js
@@ -1,74 +1,83 @@
 import React, { Component } from "react";
-import { Container, FormGroup,  } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import {
     Typography,
     FormControl,
     TextField,
     MenuItem,
+    FormGroup,
     FormControlLabel,
     Checkbox,
+    FormLabel,
     Button
 } from "@material-ui/core";
 import { Row } from "react-bootstrap";
+import "./style.css";
 
 const postCategories = ["Internship", "Project", "Announcement"];
 
-const dspaces = ["DSpace-1", "DSpace-2", "DSpace-3"];
+const dspaces = ["d-space1","d-space2","d-space3"];
 
 export class NewPost extends Component {
     render() {
         return (
         <Container>
-            <Row>
-            <Typography variant="h1"> 
-                New Post 
-            </Typography>
-            </Row>
-            <Row>
-            <FormControl>
-                <TextField
-                label="Post Title"
-                id="new-post"
-                placeholder="Post Title"
-                variant="outlined"
-                required
-                />
-                <TextField
-                label="Description"
-                id="post-description"
-                placeholder="Description"
-                variant="outlined"
-                required
-                multiline
-                rows="10"
-                />
-                <TextField
-                id="select-category"
-                select
-                label="Category"
-                helperText="Please select post category"
-                >
-                {postCategories.map(option => (
-                    <MenuItem key={option} value={option}>
-                    {option}
-                    </MenuItem>
-                ))}
-                </TextField>
-                <FormGroup>
-                {dspaces.map(dspace => (
-                    <FormControlLabel
-                    label={dspace}
-                    control={<Checkbox value={dspace}>{dspace}</Checkbox>}
-                    />
-                ))}
-                </FormGroup>
-            </FormControl>
+            <Typography variant="h1">New Post</Typography>
+            <Row className="form-div">
+                <FormControl > 
+                        <TextField
+                        label="New Post"
+                        id="new-post"
+                        placeholder="Post Title"
+                        variant="outlined"
+                        required
+                        /> 
+
+                        <TextField
+                        label="Description"
+                        id="post-description"
+                        placeholder="Description"
+                        variant="outlined"
+                        required
+                        multiline
+                        rows="10"
+                        
+                        /> 
+
+                        <TextField
+                        id="select-category"
+                        select
+                        label="Category"
+                        helperText="Please select post category"
+                        className="new-post-form-field"
+                        > 
+                        {postCategories.map(option => (
+                            <MenuItem key={option} value={option}>
+                            {option}
+                            </MenuItem>
+                        ))} 
+                        </TextField>  
+                        
+                        <div className="checkbox-section">  
+                            <FormLabel>Select D-Spaces in which you want this post to appear</FormLabel> 
+                            <FormGroup>
+                                {dspaces.map(dspaceName => ( 
+                                    <FormControlLabel 
+                                        control={ 
+                                            <Checkbox 
+                                                value={dspaceName} 
+                                            />
+                                        } 
+                                        label={dspaceName}
+                                    />
+                                ))} 
+                            </FormGroup> 
+                        </div>
+                </FormControl>
             </Row>  
-            <Button  
-                variant="outlined" 
-                color="primary"
-            >  
-                Submit Post
+
+            <Button variant="outlined" color="primary" href="#">
+                Submit
             </Button>
         </Container>
         );
