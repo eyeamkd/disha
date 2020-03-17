7c51900f83b236d59907a394b3348d58a53eec49
eyeamkd <Kunal@2297>
2020-03-18 00:53:50 +0530

    D-Space Profile design half done

--- src/components/Dspaces/index.js
+++ src/components/Dspaces/index.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
+import DspaceHeader from './DspaceHeader'
+import { Container, Typography } from '@material-ui/core';
+import { Row, Col } from 'react-bootstrap';
 
 export class Dspaces extends Component {
     render() {
-        return (
-            <div>
-                <h1>Dspaces</h1>
-            </div>
+        return ( 
+            <Container fluid>  
+                <Row> 
+                    <Col>
+                        <Typography variant="h1">D-Space Title</Typography>
+                    </Col>
+                </Row> 
+                <Row> 
+                    <Col>  
+                        <DspaceHeader/>
+                    </Col>
+                </Row>
+            </Container>
+            
         )
     }
 }
