fced03692a91b0d516056c1e28ae507e41987053
eyeamkd <Kunal@2297>
2020-04-18 02:35:17 +0530

    Fixed your Dspaces error

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,8 @@
 // Set this to true for production
-var doCache = false;
+var doCache = true;
 
 // Name our cache
-var CACHE_NAME = 'my-pwa-cache-v1';
+var CACHE_NAME = 'disha-cache-v1';
 
 // Delete old caches that are not our current one!
 window.self.addEventListener("activate", event => {
