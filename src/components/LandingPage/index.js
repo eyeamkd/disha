8aaf038d2cae0eafb1440ae2230e282930e33d81
Kaustubh <kaustubh.e@gmail.com>
2020-04-17 12:58:08 +0530

    final fix landing page

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -4,7 +4,7 @@ import { Line } from 'react-lineto';
 import FadeAnimation from 'react-fade-animation';
 import Button from '@material-ui/core/Button';
 import { Link } from "react-router-dom";
-import { Fade } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
 import MapImage from './../../assets/map.jpg'
 import Grid from '@material-ui/core/Grid';
 import Logo from './../Logo/Logo';
@@ -28,32 +28,31 @@ class LandingPage extends Component {
     render() {
         const {classes} = this.props;
         return (
-            <Col>
-                <Row>
-                    <Col>
-                        <center>
-                            <Logo height="500" width="500"/>
-                        </center>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <center>
-                            <Link to="/SignIn">
-                                <Button
-                                    type="submit"
-                                    variant="contained"
-                                    color="secondary"
-                                    className="submit button"
-                                    size="large"
-                                >
-                                    <div id="textColor">Get Started</div>
-                                </Button>
-                            </Link>
-                        </center>
-                    </Col>
-                </Row>
-            </Col>
+            <div>
+            <br/>
+            <br/>
+            <br/>
+            <Box display="flex" justifyContent="center">
+                <Logo height="327" width="250"/>
+            </Box>
+            <br/>
+            <br/>
+            <Grid container direction="row" justify="center" fullWidth>
+                <Link to="/SignIn">
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="secondary"
+                        className="button"
+                        size="large"
+                        fullWidth
+                    >
+                        <div id="textColor">Get Started</div>
+                    </Button>
+                </Link>
+            </Grid>
+
+            </div>
         )
     }
 }
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import DishaLogo from '../../assets/dishaTransparent.png';
+import DishaLogo from '../../assets/dishaTransparentThree.png';
+import "./styles.css";
 
 export default class Logo extends React.Component {
     render() {
         return(
-            <img src={DishaLogo} height={this.props.height} width={this.props.width} />
+            <img src={DishaLogo} height={this.props.height} width={this.props.width} className="logo-left"/>
         );
     }
 }
