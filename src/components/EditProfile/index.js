181a052fc642a49bd48aebb82e6fd855ce06675e
Kaustubh <kaustubh.e@gmail.com>
2020-06-24 21:22:34 +0530

    WIP edit profile

--- src/components/EditProfile/index.js
+++ src/components/EditProfile/index.js
@@ -17,11 +17,10 @@ import { Row } from "react-bootstrap";
 import { database } from '../../firebase/firebase.utils';
 import "./style.css";
 import { Redirect } from "react-router-dom";
-import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 const postCategories = ["Internship", "Project", "Events"];
-let dspaces = [];
+let posts = [];
 
 
 
@@ -45,25 +44,22 @@ export default class EditProfile extends Component {
             dataSubmittingError: '',
             dSpaces: [],
             dspaceListArrived: false,
-            userDetails: null,
+            currentUserInfo: null,
             userDataReceived: false,
             postsUpdated: false,
             theme: 'snow'
         }
-        this.handleChange = this.handleChange.bind(this)
-        //     this.getDspaces();
-        this.getUserDetails();
+        
     }
 
 
     componentDidMount() {
-        // console.log("CDM fired");
-        this.getDspaces();
         this.getUserDetails();
+        this.getPosts()
     }
 
     componentWillUnmount() {
-        dspaces = [];
+        posts = [];
     }
 
     getPosts=()=>{
@@ -111,7 +107,11 @@ export default class EditProfile extends Component {
     }
 
     updatePostData =() => {
-        
+        this.state.allPosts.forEach(post => {
+            if(post.author === this.state.currentUserInfo.firstName + " " + this.state.currentUserInfo.lastName) {
+                console.log(post)
+            }
+        })
     }
 
 
@@ -128,42 +128,16 @@ export default class EditProfile extends Component {
     getUserDetails = () => {
         let currentUserInfo = localStorage.getItem('currentUserInfo');
         let userData = JSON.parse(currentUserInfo);
-        this.setState({ userDetails: doc.data(), userDataReceived: true })
+        this.setState({ currentUserInfo: userData, userDataReceived: true })
     }
 
-    postData = () => {
-        const postUrl = this.addPostUrl()
-        const name = this.state.userDetails.firstName + " " + this.state.userDetails.lastName
-        // console.log('name', this.state.userDetails.rollNumber)
-        // console.log('rollNumber', rollNumber)
-        const newPostData = {
-            title: this.state.postTitle,
-            description: this.state.postDescription,
-            category: this.state.postCategory,
-            dSpaces: this.state.dSpaces,
-            timeStamp: this.getCurrentDate(),
-            author: name,
-            postUrl: postUrl,
-            authorRollNumber: this.state.userDetails.rollNumber,
-            likes: 0
-
-        }
-        database.collection('posts').add(newPostData)
-            .then((docRef) => { this.setState({ dataSubmittedSuccessfully: true }) })
-            .catch(err => {
-                this.setState({
-                    dataSubmittingError: 'Oof! there was an error! Please try posting after some time',
-                    onDataSubmitting: false
-                })
-            })
-    }
 
     render() {
         // console.log(this.state);
         if (this.state.dataSubmittedSuccessfully) {
             return (<Redirect to="/post-submitted" />);
         }
-        else if (this.state.userDataReceived === false || this.state.postsUpdated === false) {
+        else if (this.state.userDataReceived === false && this.state.postsUpdated === false) {
             return (
                 <div style={{
                     position: 'absolute', left: '50%', top: '50%',
@@ -183,8 +157,7 @@ export default class EditProfile extends Component {
                             <TextField
                                 label="First Name"
                                 id="firstName"
-                                placeholder="First Name"
-                                value={this.state.userDetails.firstName}
+                                placeholder={this.state.currentUserInfo.firstName}
                                 variant="outlined"
                                 required
                                 onChange={this.handleFirstNameChange}
@@ -193,14 +166,13 @@ export default class EditProfile extends Component {
                             <TextField
                                 label="Last Name"
                                 id="lastName"
-                                placeholder="Last Name"
+                                placeholder={this.state.currentUserInfo.lastName}
                                 variant="outlined"
                                 required
                                 onChange={this.handleLastNameChange}
-                                value={this.state.userDetails.lastName}
                                 error={this.state.isLastNameInvalid}
                             />
-                            <button onClick={this.handleTextChange}>Clcikd</button>
+                            <button onClick={this.handleSubmit}>Clcikd</button>
 
 
                         </FormControl>
