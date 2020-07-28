8871b5aeecaddb2a2d2feb98e0432e31f8c2f2ac
Kunal Dubey <kunaldubey2297@gmail.com>
2020-07-29 00:28:16 +0530

    Custom upload button for image using react reference

--- src/shared/ImageUploadComponent.jsx
+++ src/shared/ImageUploadComponent.jsx
@@ -1,29 +1,18 @@
-import React, { Component, useState, useEffect } from "react";
-import { DropzoneArea } from "material-ui-dropzone";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import styled from "styled-components";
+import Button from "@material-ui/core/Button";
+import React, { useEffect, useState } from "react";
 import "react-image-crop/dist/ReactCrop.css";
-import ReactCrop from "react-image-crop";
+import styled from "styled-components";
 const ImageWrapper = styled.div`
   height: 200px;
   width: 250px;
   margin: 15px;
 `;
 const reader = new FileReader();
-const useStyles = makeStyles((theme) =>
-  createStyles({
-    previewChip: {
-      minWidth: 250,
-      maxWidth: 250,
-      maxHeight: 250,
-      minHeight: 250,
-    },
-  })
-);
+const inputButtonReference = React.createRef();
 export const ImageUploadComponent = (props) => {
-  const [files, setfiles] = useState([]); 
-  const [filePath, setfilePath] = useState(null);
-  const [crop, setCrop] = useState({ aspect: 16 / 9 });
+  const [files, setfiles] = useState([]);
+  // const [filePath, setfilePath] = useState(null);
+  // const [crop, setCrop] = useState({ aspect: 16 / 9 });
   useEffect(() => {
     if (!!files[0]) {
       reader.onload = (e) => {
@@ -31,45 +20,40 @@ export const ImageUploadComponent = (props) => {
         props.onImageUpload(e.target.result);
       };
       reader.readAsDataURL(files[0]);
-      console.log(files); 
-      setfilePath(files[0].path);
+      console.log(files);
+      
     }
   }, [files]);
-  const classes = useStyles(); 
+
+  const onImageUploaded = (event) => {
+    setfiles(event.target.files[0]);
+  };
+  
+
+  const uploadButtonClicked = () => {
+    console.log("Upload Button Clicked");
+    inputButtonReference.current.click();
+  };
   return (
     <div>
-      <ImageWrapper>
-        <DropzoneArea
-          filesLimit={1}
-          onChange={(files) => setfiles(files)}
-          previewChipProps={{ classes: { root: classes.previewChip } }}
-          dropzoneText="Drop Image or Click here to upload"
-          acceptedFiles={["image/*"]}
-        />
-      </ImageWrapper>
-      {/* {filePath && ( */}
-        <ReactCrop
-          src={filePath}
-          crop={crop}
-          ruleOfThirds
-          onImageLoaded={onImageLoaded()}
-          onComplete={onCropComplete()}
-          onChange={onCropChange()}
-        />
-      {/* )} */}
+      <Button
+        type="submit"
+        variant="contained"
+        color="secondary"
+        className="submit"
+        onClick={uploadButtonClicked}
+      >
+        UPLOAD
+      </Button>
+
+      <input
+        type="file"
+        onChange={onImageUploaded}
+        style={{ display: "none" }}
+        ref={inputButtonReference}
+      />
     </div>
   );
-}; 
-
-const onImageLoaded=()=>{ 
-    console.log("Image loaded");
-    
-}  
-const onCropComplete=()=>{
-    console.log("Crop Complete");
-} 
-const onCropChange=()=>{
-    console.log("Crop Change");
-} 
+};
 
 export default ImageUploadComponent;
