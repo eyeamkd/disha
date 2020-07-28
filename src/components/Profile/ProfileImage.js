a10d5bd5c70748f31a03392a1357191783db4da6
Kunal Dubey <kunaldubey2297@gmail.com>
2020-07-28 23:34:02 +0530

    images wip

--- src/components/Profile/ProfileImage.js
+++ src/components/Profile/ProfileImage.js
@@ -3,7 +3,10 @@ import { Avatar, Typography } from "@material-ui/core";
 
 export class ProfileImage extends Component {
   constructor(props) {
-    super(props);
+    super(props); 
+    this.state={ 
+
+    }
   }
   render() {
     let scale = this.props.scale;
@@ -11,8 +14,9 @@ export class ProfileImage extends Component {
       <div>
         <Avatar
           variant={this.props.variant}
-          sizes="width:100; height:100"
-          style={Object.assign({
+          sizes="width:100; height:100" 
+          onMouseEnter={()=>{console.log("Mouse Entereed")}}
+          style={Object.assign({ 
             width: scale,
             height: scale
           })}
--- src/shared/ImageUploadComponent.jsx
+++ src/shared/ImageUploadComponent.jsx
@@ -1,47 +1,75 @@
-import React, { Component, useState, useEffect} from 'react'
-import { DropzoneArea } from 'material-ui-dropzone';  
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import styled from 'styled-components'
-const ImageWrapper = styled.div` 
-        height:200px; 
-        width:250px; 
-        margin:15px;
-    `  
+import React, { Component, useState, useEffect } from "react";
+import { DropzoneArea } from "material-ui-dropzone";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import styled from "styled-components";
+import "react-image-crop/dist/ReactCrop.css";
+import ReactCrop from "react-image-crop";
+const ImageWrapper = styled.div`
+  height: 200px;
+  width: 250px;
+  margin: 15px;
+`;
 const reader = new FileReader();
-const useStyles = makeStyles((theme) => createStyles({
-        previewChip: {
-          minWidth: 250,
-          maxWidth: 250, 
-          maxHeight:250,
-          minHeight:250
-        }
-      }),
-    )
-export const ImageUploadComponent = (props) => {  
-    const [files, setfiles] = useState([]) 
-    useEffect(() => { 
-        if(!!files[0]){  
-            reader.onload  = (e)=>{ 
-               console.log(e.target.result); 
-               props.onImageUpload(e.target.result)
-            } 
-            reader.readAsDataURL(files[0])
-            console.log(files);
-            
-        }
-    }, [files])
-    const classes = useStyles()
-        return( 
-            <ImageWrapper>  
-                <DropzoneArea   
-                filesLimit={1}
-                onChange={(files)=>setfiles(files)} 
-                previewChipProps={{classes: { root: classes.previewChip } }} 
-                dropzoneText="Drop Image or Click here to upload" 
-                acceptedFiles={['image/*']}
-                />
-            </ImageWrapper>
-        )
-}
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    previewChip: {
+      minWidth: 250,
+      maxWidth: 250,
+      maxHeight: 250,
+      minHeight: 250,
+    },
+  })
+);
+export const ImageUploadComponent = (props) => {
+  const [files, setfiles] = useState([]); 
+  const [filePath, setfilePath] = useState(null);
+  const [crop, setCrop] = useState({ aspect: 16 / 9 });
+  useEffect(() => {
+    if (!!files[0]) {
+      reader.onload = (e) => {
+        console.log(e.target.result);
+        props.onImageUpload(e.target.result);
+      };
+      reader.readAsDataURL(files[0]);
+      console.log(files); 
+      setfilePath(files[0].path);
+    }
+  }, [files]);
+  const classes = useStyles(); 
+  return (
+    <div>
+      <ImageWrapper>
+        <DropzoneArea
+          filesLimit={1}
+          onChange={(files) => setfiles(files)}
+          previewChipProps={{ classes: { root: classes.previewChip } }}
+          dropzoneText="Drop Image or Click here to upload"
+          acceptedFiles={["image/*"]}
+        />
+      </ImageWrapper>
+      {/* {filePath && ( */}
+        <ReactCrop
+          src={filePath}
+          crop={crop}
+          ruleOfThirds
+          onImageLoaded={onImageLoaded()}
+          onComplete={onCropComplete()}
+          onChange={onCropChange()}
+        />
+      {/* )} */}
+    </div>
+  );
+}; 
 
-export default ImageUploadComponent
+const onImageLoaded=()=>{ 
+    console.log("Image loaded");
+    
+}  
+const onCropComplete=()=>{
+    console.log("Crop Complete");
+} 
+const onCropChange=()=>{
+    console.log("Crop Change");
+} 
+
+export default ImageUploadComponent;
