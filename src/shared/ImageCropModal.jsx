5a1dd5a578a175c3c08d222b098dac88202ee626
Kunal Dubey <kunaldubey2297@gmail.com>
2020-08-02 23:06:51 +0530

    able to upload cropped image into firestore

--- src/shared/ImageCropModal.jsx
+++ src/shared/ImageCropModal.jsx
@@ -1,8 +1,10 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import ReactCrop from "react-image-crop";
 import "react-image-crop/dist/ReactCrop.css";
+import { Button } from "@material-ui/core"; 
+import { storageRef, database } from "../firebase/firebase.utils";
 
 function rand() {
   return Math.round(Math.random() * 20) - 10;
@@ -34,12 +36,12 @@ export default function ImageCropModal(props) {
   const classes = useStyles();
   // getModalStyle is not a pure function, we roll the style only on the first render
   const [modalStyle] = useState(getModalStyle);
-  const [open, setOpen] = useState(false); 
-  const [croppedImageUrl, setCroppedImageUrl] = useState('');
-  const [src, setSrc] = useState('');
-  const [crop, setCrop] = useState(''); 
-  const [imageRef, setImageRef] = useState(''); 
-  const [fileUrl, setFileUrl] = useState('');
+  const [open, setOpen] = useState(false);
+  const [croppedImageUrl, setCroppedImageUrl] = useState("");
+  const [src, setSrc] = useState("");
+  const [crop, setCrop] = useState("");
+  const [imageRef, setImageRef] = useState("");
+  const [file, setFile] = useState("");
 
   const handleOpen = () => {
     setOpen(true);
@@ -47,43 +49,41 @@ export default function ImageCropModal(props) {
 
   const handleClose = () => {
     setOpen(false);
-  }; 
+  };
 
-  const onImageLoaded = image => {
+  const onImageLoaded = (image) => {
     setImageRef(image);
   };
 
-  const onCropComplete = crop => {
+  const onCropComplete = (crop) => {
     makeClientCrop(crop);
-  }; 
-
-
+  };
 
   const onCropChange = (crop, percentCrop) => {
     // You could also use percentCrop:
     // this.setState({ crop: percentCrop });
-    // this.setState({ crop }); 
+    // this.setState({ crop });
     setCrop(crop);
   };
 
-  const makeClientCrop=async(crop)=>{
+  const makeClientCrop = async (crop) => {
     if (imageRef && crop.width && crop.height) {
       const croppedImageUrl = await getCroppedImg(
         imageRef,
         crop,
         props.file.name
-      ); 
+      );
       setCroppedImageUrl(croppedImageUrl);
     }
-  }
+  };
 
-  const getCroppedImg=(image, crop, fileName)=>{
-    const canvas = document.createElement('canvas');
+  const getCroppedImg = (image, crop, fileName) => {
+    const canvas = document.createElement("canvas");
     const scaleX = image.naturalWidth / image.width;
     const scaleY = image.naturalHeight / image.height;
     canvas.width = crop.width;
     canvas.height = crop.height;
-    const ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext("2d");
 
     ctx.drawImage(
       image,
@@ -95,30 +95,44 @@ export default function ImageCropModal(props) {
       0,
       crop.width,
       crop.height
-    ); 
+    );
 
-    return new Promise((resolve,reject)=>{
-       resolve(canvas.toDataURL('image/jpeg'));
-    })
+    return new Promise((resolve, reject) => {
+      resolve(canvas.toDataURL("image/jpeg"));
+    });
+  };
 
-    // return new Promise((resolve, reject) => { 
-    
-    //   canvas.toBlob(blob => {
-    //     if (!blob) {
-    //       //reject(new Error('Canvas is empty'));
-    //       console.error('Canvas is empty');
-    //       return;
-    //     }
-    //     blob.name = fileName;
-    //     window.URL.revokeObjectURL(fileUrl);
-    //     setFileUrl(window.URL.createObjectURL(blob));
-    //     resolve(fileUrl);
-    //   }, 'image/jpeg');
-    // });
-  }
+  const uploadCroppedImage = () => {
+    fetch(src)
+      .then((res) => res.blob)
+      .then((blob) => {
+        const file = new File([blob], props.file.name, { type: "image/png" });
+        setFile(file);
+        storeImageOnFireStore(file);
+      });
+  };
 
-
-
+  const storeImageOnFireStore = (file) => {
+    let userRollNumber = JSON.parse(localStorage.getItem("currentUserInfo"))
+      .rollNumber;
+    //dangerous if user deletes the localstorage and uploads the image
+    let userProfileImageStorageReference = storageRef.child(
+      `profile-images/${userRollNumber}-${file.name}`
+    );
+    userProfileImageStorageReference.put(file).then((snapshot) => {
+      console.log("Uploaded Snapshot ", snapshot);
+      if (snapshot.state === "success") {
+        let userDocRef = database
+          .collection("users")
+          .doc(localStorage.getItem("currentUserId"));
+        userDocRef
+          .update({ profileImagePath: snapshot.metadata.fullPath })
+          .then((res) => {
+            console.log("Image Uploaded Successfully!!", res);
+          });
+      }
+    });
+  };
 
   const body = (
     <div style={modalStyle} className={classes.paper}>
@@ -133,12 +147,15 @@ export default function ImageCropModal(props) {
         onChange={onCropChange}
       />
       {croppedImageUrl && (
-        <img alt="Crop" style={{ maxWidth: "100%" }} src={croppedImageUrl} />
+        <div>
+          <img alt="Crop" style={{ maxWidth: "100%" }} src={croppedImageUrl} />
+          <Button onClick={uploadCroppedImage}>Set as Profile Image</Button>
+        </div>
       )}
     </div>
   );
 
-  return (  
+  return (
     <div>
       <Modal
         open={props.open}
--- src/shared/ImageUploadComponent.jsx
+++ src/shared/ImageUploadComponent.jsx
@@ -10,18 +10,18 @@ const ImageWrapper = styled.div`
   margin: 15px;
 `;
 const reader = new FileReader();
-const inputButtonReference = React.createRef(); 
+const inputButtonReference = React.createRef();
 
-//display image on modal
-//react crop operations on modal 
-// display uploaded image as a profile image everywhere 
-// repeat the same functionality for Dspaces 
-// modify D-space search cards to display the image  
+//  display image on modal
+//  react crop operations on modal
+//  display uploaded image as a profile image everywhere
+//  repeat the same functionality for Dspaces
+//  modify D-space search cards to display the image
 
 export const ImageUploadComponent = (props) => {
   const [files, setfiles] = useState([]);
-  const [isImageUploaded, setIsImageUploaded] = useState(false); 
-  const [src, setSrc] = useState('');
+  const [isImageUploaded, setIsImageUploaded] = useState(false);
+  const [src, setSrc] = useState("");
   // const [filePath, setfilePath] = useState(null);
   // const [crop, setCrop] = useState({ aspect: 16 / 9 });
   useEffect(() => {
@@ -33,71 +33,70 @@ export const ImageUploadComponent = (props) => {
       reader.readAsDataURL(files[0]);
       console.log(files);
     }
-  }, [files,isImageUploaded]); 
-  
+  }, [files, isImageUploaded]);
+
   const onSelectFile = (file) => {
     if (file) {
       const reader = new FileReader();
-      reader.addEventListener('load', () =>
-        // this.setState({ src: reader.result }) 
+      reader.addEventListener("load", () =>
+        // this.setState({ src: reader.result })
         setSrc(reader.result)
       );
       reader.readAsDataURL(file);
+      setIsImageUploaded(true);
     }
-  }; 
+  };
 
-  const storeImageOnFireStore=(file)=>{
-    let userRollNumber = JSON.parse(localStorage.getItem('currentUserInfo')).rollNumber; 
-    //dangerous if user deletes the localstorage and uploads the image 
-    let userProfileImageStorageReference  = storageRef.child(`profile-images/${userRollNumber}-${file.name}`);
-    userProfileImageStorageReference.put(file).then((snapshot) => {
-      console.log("Uploaded Snapshot ", snapshot);  
-      if(snapshot.state==='success'){
-        let userDocRef = database.collection('users').doc(localStorage.getItem('currentUserId')); 
-        userDocRef.update({profileImagePath : snapshot.metadata.fullPath}).then((res)=>{
-            console.log("Image Uploaded Successfully!!",res); 
-            setIsImageUploaded(true); 
-        })
-      } 
-  }); 
-}
+  //   const storeImageOnFireStore=(file)=>{
+  //     let userRollNumber = JSON.parse(localStorage.getItem('currentUserInfo')).rollNumber;
+  //     //dangerous if user deletes the localstorage and uploads the image
+  //     let userProfileImageStorageReference  = storageRef.child(`profile-images/${userRollNumber}-${file.name}`);
+  //     userProfileImageStorageReference.put(file).then((snapshot) => {
+  //       console.log("Uploaded Snapshot ", snapshot);
+  //       if(snapshot.state==='success'){
+  //         let userDocRef = database.collection('users').doc(localStorage.getItem('currentUserId'));
+  //         userDocRef.update({profileImagePath : snapshot.metadata.fullPath}).then((res)=>{
+  //             console.log("Image Uploaded Successfully!!",res);
+
+  //         })
+  //       }
+  //   });
+  // }
   const onImageUploaded = (event) => {
-    let file = event.target.files[0];   
-      onSelectFile(file);
-      storeImageOnFireStore(file);
-      setfiles(file);
-    }
+    let file = event.target.files[0];
+    onSelectFile(file);
+    // storeImageOnFireStore(file);
+    setfiles(file);
+  };
 
   const uploadButtonClicked = () => {
     console.log("Upload Button Clicked");
     inputButtonReference.current.click();
   };
-  return (   
-    <div className="main-class"> 
-    {  
-      isImageUploaded 
-      ? 
-      <ImageCropModal open={isImageUploaded} file={files} src={src}/> 
-      : 
-      <div className="image-upload-button"> 
-      <Button
-        type="submit"
-        variant="contained"
-        color="secondary"
-        className="submit"
-        onClick={uploadButtonClicked}
-      >
-        UPLOAD
-      </Button>
+  return (
+    <div className="main-class">
+      {isImageUploaded ? (
+        <ImageCropModal open={isImageUploaded} file={files} src={src} />
+      ) : (
+        <div className="image-upload-button">
+          <Button
+            type="submit"
+            variant="contained"
+            color="secondary"
+            className="submit"
+            onClick={uploadButtonClicked}
+          >
+            UPLOAD
+          </Button>
 
-      <input
-        type="file"
-        onChange={onImageUploaded}
-        style={{ display: "none" }}
-        ref={inputButtonReference}
-      />
-    </div> 
-    } 
+          <input
+            type="file"
+            onChange={onImageUploaded}
+            style={{ display: "none" }}
+            ref={inputButtonReference}
+          />
+        </div>
+      )}
     </div>
   );
 };
